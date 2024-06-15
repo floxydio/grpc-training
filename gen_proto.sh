aeffc317f84b69d36b65429256a48f85985ec8dd
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2024-06-15 17:23:13 +0700

    folder

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,2 +1,2 @@
 protoc --go_out=./pkg proto/*.proto --go-grpc_out=./pkg
-protoc --dart_out=grpc:shopproto/pkg -Iproto proto/*.proto google/protobuf/timestamp.proto
+protoc --dart_out=grpc:shopproto/lib/pkg -Iproto proto/*.proto google/protobuf/timestamp.proto
